23af9f7983fbb763a0ac1b6a7e0a8a0d707cc6cf
mehmetcanaygun <mehmetcanaygun@gmail.com>
2021-07-17 22:35:23 +0300

    Add Pagination

---
+++ src/components/ThePagination.vue
@@ -0,0 +1,55 @@
+<template>
+  <div
+    v-if="pagination"
+    class="pagination"
+  >
+    <button
+      v-if="pagination.previous"
+      @click="$emit('get-page', pagination.previous)"
+    ><i class="fas fa-chevron-left"></i></button>
+    <button
+      v-if="pagination.next"
+      @click="$emit('get-page', pagination.next)"
+    ><i class="fas fa-chevron-right"></i></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ThePagination",
+  props: {
+    pagination: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+// Import
+@import "../style/variables";
+
+.pagination {
+  width: 100%;
+  height: 30px;
+  margin-bottom: 20px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 10px;
+
+  button {
+    height: 100%;
+    width: 30px;
+    border: none;
+    background-color: $color-light;
+    border-radius: 6px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: $color-primary;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="home">
+    <ThePagination
+      :pagination="pagination"
+      @get-page="getPage"
+    />
     <StarshipList :starships="starships" />
+    <ThePagination
+      :pagination="pagination"
+      @get-page="getPage"
+    />
   </div>
 </template>
 
 <script>
 import StarshipList from "../components/StarshipList";
+import ThePagination from "../components/ThePagination";
 
 export default {
   name: "Home",
   components: {
     StarshipList,
+    ThePagination,
   },
   data() {
     return {
@@ -27,9 +37,25 @@ export default {
 
         const { count, next, previous, results } = data;
 
-        console.log(results[0]);
+        // console.log(results[0]);
 
         return [{ count, next, previous }, results];
+      } catch (err) {
+        console.log(err.message);
+      }
+    },
+    async getPage(url) {
+      try {
+        const res = await fetch(url);
+
+        const data = await res.json();
+
+        const { count, next, previous, results } = data;
+
+        [this.pagination, this.starships] = [
+          { count, next, previous },
+          results,
+        ];
       } catch (err) {
         console.log(err.message);
       }
